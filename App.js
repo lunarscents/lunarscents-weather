96187fe05e5de74ba25888c15ee53e0fc3d200c1
lunarscents <lunarscents7@gmail.com>
2019-11-12 23:46:41 +0900

    [component] - update App and Loading component

--- App.js
+++ App.js
@@ -2,10 +2,29 @@ import React from "react";
 import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import axios from "axios";
+import Weather from "./Weather";
+
+const API_KEY = "af52a321959f67fdbd1c573950b1bf58";
 
 export default class extends React.Component {
   state = {
     isLoading: true
+  };
+  getWeather = async (latitude, longitude) => {
+    const {
+      data: {
+        main: { temp },
+        weather
+      }
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+    );
+    this.setState({
+      isLoading: false,
+      condition: weather[0].main,
+      temp
+    });
   };
   getLocation = async () => {
     try {
@@ -13,16 +32,20 @@ export default class extends React.Component {
       const {
         coords: { latitude, longitude }
       } = await Location.getCurrentPositionAsync();
-
-      this.setState({ isLoading: false });
+      this.getWeather(latitude, longitude);
     } catch (error) {
       Alert.alert("Can't find you.", "So sad");
     }
   };
-  componentDidMount() {}
-
-  return() {
-    const { isLoading } = this.state;
-    return isLoading ? <Loading /> : null;
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
+    );
   }
 }
--- Loading.js
+++ Loading.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
 
 export default function Loading() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello!</Text>
+      <StatusBar barStyle="dark-content" />
+      <Text style={styles.text}>Hi, Weather</Text>
     </View>
   );
 }
@@ -15,9 +16,8 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
     paddingHorizontal: 30,
     paddingVertical: 100,
-    backgroundColor: "#fdf6aa"
+    backgroundColor: "#FDF6AA"
   },
-
   text: {
     color: "#2c2c2c",
     fontSize: 30
